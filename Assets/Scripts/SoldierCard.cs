f5751b07bd6f199b9aaef1ed95d8028e2c78a50b
AGYagy <43157278+AGYagy@users.noreply.github.com>
2019-01-01 22:56:28 +0800

    Update SoldierCard.cs

--- Assets/Scripts/SoldierCard.cs
+++ Assets/Scripts/SoldierCard.cs
@@ -11,11 +11,19 @@ public class SoldierCard : Card {
     private TroopBarManager troopBarManager;
     public void Initialize(Soldier soldier)
     {
-
+        manager = GameObject.Find("CardManager");
+        template = soldier;
+        transform.Find("CardType").gameObject.GetComponent<Text>().text = template.type;
+        soldierBarManager = GameObject.Find("SoldierManager").GetComponent<SoldierBarManager>();
+        troopBarManager = GameObject.Find("TroopManager").GetComponent<TroopBarManager>();
     }
 
     override public void Remove()
     {
-
+        if (troopBarManager.HasSelected() != true) return;
+        soldierBarManager.Add(template);
+        gameObject.GetComponent<EventTrigger>().enabled = false;
+        manager.GetComponent<HorizontalBarWithEffectManager>().Delete(gameObject);
+        StartCoroutine(Fade(0.2f));
     }
 }
